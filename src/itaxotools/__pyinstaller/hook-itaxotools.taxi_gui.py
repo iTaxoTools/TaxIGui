720cad4416b1baaa990f4e53155a6d62fffe3469
StefanPatman <stefanpatman91@gmail.com>
2023-04-30 14:30:00 +0300

    Fix pyinstaller hooks for task submodules

--- src/itaxotools/__pyinstaller/hook-itaxotools.taxi_gui.py
+++ src/itaxotools/__pyinstaller/hook-itaxotools.taxi_gui.py
@@ -1,3 +1,6 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 datas = collect_data_files('itaxotools.taxi_gui')
+
+hiddenimports = collect_submodules(
+    'itaxotools.taxi_gui.tasks', filter=lambda name: True)
