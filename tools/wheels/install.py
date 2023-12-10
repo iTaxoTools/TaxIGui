de77af0b0ee76d52710deea129c7e136949e0867
Stefanos Patmanidis <stefanpatman91@gmail.com>
2023-12-10 22:45:54 +0200

    Update macos frozen wheels

--- tools/wheels/install.py
+++ tools/wheels/install.py
@@ -5,6 +5,7 @@ import tempfile
 from pathlib import Path
 from urllib.request import urlopen
 
+from delocate.fuse import fuse_wheels
 import yaml
 
 
@@ -21,29 +22,63 @@ def verify_file(file_path, expected_hash):
     return calculated_hash == expected_hash
 
 
-def download_file(egg, url, tmp_dir: Path):
+def download_file(egg, url, tmp_dir: Path) -> Path:
     with urlopen(url) as response:
-        if response.getcode() == 200:
-            file_name = Path(url).name
-            file_path = tmp_dir / file_name
-            with open(file_path, "wb") as file:
-                file.write(response.read())
-            return file_path
-        else:
+        if response.getcode() != 200:
             raise Exception(
                 f"Failed to download wheel for egg: {egg}. HTTP status code: {response.getcode()}"
             )
+        file_name = Path(url).name
+        file_path = tmp_dir / file_name
+        with open(file_path, "wb") as file:
+            file.write(response.read())
+        return file_path
 
 
-def download_wheel(wheel: dict, tmp_dir: Path):
+def download_universal2_wheel(wheel: dict, tmp_dir: Path):
     egg = wheel["egg"]
-    url = wheel["url"]
-    hash = wheel["sha256"]
+    url = wheel["universal2_url"]
+    hash = wheel["universal2_sha256"]
 
     print(f"Downloading wheel for {egg}...")
     file_path = download_file(egg, url, tmp_dir)
     if not verify_file(file_path, hash):
         raise Exception(f"Wheel hash does not match for: {egg}")
+
+
+def download_and_fuse_wheels(wheel: dict, tmp_dir: Path):
+    egg = wheel["egg"]
+    arm64_url = wheel["arm64_url"]
+    arm64_hash = wheel["arm64_sha256"]
+    x86_64_url = wheel["x86_64_url"]
+    x86_64_hash = wheel["x86_64_sha256"]
+
+    print(f"Downloading arm64 wheel for {egg}...")
+    arm64_path = download_file(egg, arm64_url, tmp_dir)
+    if not verify_file(arm64_path, arm64_hash):
+        raise Exception(f"Wheel hash (arm64) does not match for: {egg}")
+
+    print(f"Downloading x86_64 wheel for {egg}...")
+    x86_64_path = download_file(egg, x86_64_url, tmp_dir)
+    if not verify_file(x86_64_path, x86_64_hash):
+        raise Exception(f"Wheel hash (x86_64) does not match for: {egg}")
+
+    print(f"Fusing universal2 wheel for {egg}...")
+    stem = arm64_path.name.split("-macosx")[0]
+    universal2_path = tmp_dir / f"{stem}-macosx_11_0_universal2.whl"
+    fuse_wheels(arm64_path, x86_64_path, universal2_path)
+
+    arm64_path.unlink()
+    x86_64_path.unlink()
+
+
+def download_wheel(wheel: dict, tmp_dir: Path):
+    egg = wheel["egg"]
+    if "universal2_url" in wheel:
+        return download_universal2_wheel(wheel, tmp_dir)
+    if "arm64_url" in wheel and "x86_64_url" in wheel:
+        return download_and_fuse_wheels(wheel, tmp_dir)
+    raise Exception(f"No wheel resolution for: {egg}")
 
 
 def download_wheels(wheels: list[dict], tmp_dir: Path):
@@ -53,10 +88,10 @@ def download_wheels(wheels: list[dict], tmp_dir: Path):
 
 def install_all(tmp_dir: Path):
     wheels = list(tmp_dir.glob("*.whl"))
-    cmd = [sys.executable, "-m", "pip", "install"]
-    cmd.extend(str(wheel) for wheel in wheels)
+    cmd = [sys.executable, "-m", "pip", "install", "--force-reinstall"]
+    cmd.extend(wheel.name for wheel in wheels)
     print("Running:", " ".join(cmd))
-    subprocess.check_call(cmd)
+    subprocess.check_call(cmd, cwd=tmp_dir)
 
 
 def main(path: str):
